c9146669ffed97df742557228710a3c033b15a26
C.O Sistemas <caiosan321@icloud.com>
2026-02-01 16:51:22 -0300

    Update database.py

--- database.py
+++ database.py
@@ -1,7 +1,10 @@
-import sqlite3
+import psycopg2
+import os
+
+DATABASE_URL = os.environ.getpostgresql://barbearia_db_k3da_user:Qc72SeyRPeoVVYvLDTRytgqJmkSbg5h2@dpg-d5vqldh4tr6s73a0dtr0-a/barbearia_db_k3da
 
 def conectar():
-    return sqlite3.connect("barbearia.db", check_same_thread=False)
+    return psycopg2.connect(DATABASE_URL)
 
 def criar_tabelas():
     con = conectar()
@@ -9,7 +12,7 @@ def criar_tabelas():
 
     c.execute("""
     CREATE TABLE IF NOT EXISTS clientes (
-        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        id SERIAL PRIMARY KEY,
         nome TEXT,
         telefone TEXT,
         plano_id INTEGER,
@@ -19,43 +22,43 @@ def criar_tabelas():
 
     c.execute("""
     CREATE TABLE IF NOT EXISTS servicos (
-        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        id SERIAL PRIMARY KEY,
         nome TEXT,
-        preco REAL
+        preco NUMERIC
     )
     """)
 
     c.execute("""
     CREATE TABLE IF NOT EXISTS produtos (
-        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        id SERIAL PRIMARY KEY,
         nome TEXT,
-        preco REAL
+        preco NUMERIC
     )
     """)
 
     c.execute("""
     CREATE TABLE IF NOT EXISTS planos (
-        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        id SERIAL PRIMARY KEY,
         nome TEXT,
-        valor REAL,
+        valor NUMERIC,
         limite INTEGER
     )
     """)
 
     c.execute("""
     CREATE TABLE IF NOT EXISTS uso_plano (
-        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        id SERIAL PRIMARY KEY,
         cliente_id INTEGER,
         servico_id INTEGER,
-        data DATETIME DEFAULT CURRENT_TIMESTAMP
+        data TIMESTAMP DEFAULT CURRENT_TIMESTAMP
     )
     """)
 
     c.execute("""
     CREATE TABLE IF NOT EXISTS agenda (
-        id INTEGER PRIMARY KEY AUTOINCREMENT,
-        data TEXT,
-        hora TEXT,
+        id SERIAL PRIMARY KEY,
+        data DATE,
+        hora TIME,
         cliente_id INTEGER,
         servico_id INTEGER
     )
@@ -65,24 +68,25 @@ def criar_tabelas():
     CREATE TABLE IF NOT EXISTS caixa_status (
         id INTEGER PRIMARY KEY,
         aberto INTEGER,
-        saldo_inicial REAL
+        saldo_inicial NUMERIC
     )
     """)
 
     c.execute("""
     CREATE TABLE IF NOT EXISTS caixa (
-        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        id SERIAL PRIMARY KEY,
         descricao TEXT,
-        valor REAL,
+        valor NUMERIC,
         tipo TEXT,
         pagamento TEXT,
-        data DATETIME DEFAULT CURRENT_TIMESTAMP
+        data TIMESTAMP DEFAULT CURRENT_TIMESTAMP
     )
     """)
 
     c.execute("""
-    INSERT OR IGNORE INTO caixa_status (id, aberto, saldo_inicial)
+    INSERT INTO caixa_status (id, aberto, saldo_inicial)
     VALUES (1, 0, 0)
+    ON CONFLICT (id) DO NOTHING
     """)
 
     con.commit()
