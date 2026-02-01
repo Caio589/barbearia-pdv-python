32a4067bacca285ca9b20b6df421266166a4a579
C.O Sistemas <caiosan321@icloud.com>
2026-02-01 18:59:14 -0300

    Update script.js

--- static/script.js
+++ static/script.js
@@ -1,11 +1,33 @@
+function mostrar(id) {
+  document.querySelectorAll(".aba").forEach(a => a.style.display = "none");
+  document.getElementById(id).style.display = "block";
+}
+
+// mostrar aba inicial
+mostrar("clientes");
+
+// CLIENTES
 function addCliente(){
   fetch("/clientes",{
     method:"POST",
     headers:{"Content-Type":"application/json"},
     body:JSON.stringify({nome:cliente.value})
-  })
+  }).then(()=>carregarClientes());
 }
 
+function carregarClientes(){
+  fetch("/clientes")
+    .then(r=>r.json())
+    .then(d=>{
+      listaClientes.innerHTML="";
+      d.forEach(c=>{
+        listaClientes.innerHTML+=`<li>${c[1]}</li>`;
+      });
+    });
+}
+carregarClientes();
+
+// AGENDA
 function agendar(){
   fetch("/agenda",{
     method:"POST",
@@ -16,5 +38,22 @@ function agendar(){
       data:ag_data.value,
       hora:ag_hora.value
     })
-  })
+  }).then(()=>carregarAgenda());
 }
+
+function carregarAgenda(){
+  fetch("/agenda")
+    .then(r=>r.json())
+    .then(d=>{
+      listaAgenda.innerHTML="";
+      d.forEach(a=>{
+        listaAgenda.innerHTML+=
+          `<li>${a[1]} - ${a[2]} (${a[3]} ${a[4]})</li>`;
+      });
+    });
+}
+carregarAgenda();
+
+// CAIXA (placeholder)
+function abrirCaixa(){ alert("Caixa aberto"); }
+function fecharCaixa(){ alert("Caixa fechado"); }
