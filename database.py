e9f5e77bfa576eba050687066fea11584a43c4e0
C.O Sistemas <caiosan321@icloud.com>
2026-02-01 09:52:28 -0300

    Update database.py

--- database.py
+++ database.py
@@ -1,1 +1,38 @@
+import sqlite3
 
+def conectar():
+    return sqlite3.connect("barbearia.db")
+
+def criar_tabelas():
+    conn = conectar()
+    cursor = conn.cursor()
+
+    cursor.execute("""
+    CREATE TABLE IF NOT EXISTS clientes (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        nome TEXT NOT NULL,
+        telefone TEXT,
+        criado_em DATETIME DEFAULT CURRENT_TIMESTAMP
+    )
+    """)
+
+    cursor.execute("""
+    CREATE TABLE IF NOT EXISTS servicos (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        nome TEXT NOT NULL,
+        preco REAL NOT NULL
+    )
+    """)
+
+    cursor.execute("""
+    CREATE TABLE IF NOT EXISTS caixa (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        descricao TEXT,
+        valor REAL,
+        tipo TEXT,
+        data DATETIME DEFAULT CURRENT_TIMESTAMP
+    )
+    """)
+
+    conn.commit()
+    conn.close()
