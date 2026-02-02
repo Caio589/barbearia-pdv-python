20363b2c39c76c5fc18d0e17095eb1e13fa4a232
C.O Sistemas <caiosan321@icloud.com>
2026-02-02 11:47:36 -0300

    Update script.js

--- static/script.js
+++ static/script.js
@@ -2,11 +2,42 @@ function mostrarTela(id) {
     document.querySelectorAll(".tela").forEach(tela => {
         tela.style.display = "none";
     });
-
-    const tela = document.getElementById(id);
-    if (tela) tela.style.display = "block";
+    document.getElementById(id).style.display = "block";
 }
 
 document.addEventListener("DOMContentLoaded", () => {
     mostrarTela("inicio");
+    carregarClientes();
 });
+
+function salvarCliente() {
+    fetch("/clientes", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({
+            nome: nomeCliente.value,
+            telefone: telCliente.value
+        })
+    })
+    .then(r => r.json())
+    .then(d => {
+        alert(d.msg);
+        nomeCliente.value = "";
+        telCliente.value = "";
+        carregarClientes();
+    });
+}
+
+function carregarClientes() {
+    fetch("/clientes")
+        .then(r => r.json())
+        .then(lista => {
+            const ul = document.getElementById("listaClientes");
+            ul.innerHTML = "";
+            lista.forEach(c => {
+                const li = document.createElement("li");
+                li.innerText = `${c[1]} — ${c[2]}`;
+                ul.appendChild(li);
+            });
+        });
+}
