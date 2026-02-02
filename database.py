8c5b983058f1f4da663ccbc4c71a771c918c4da9
C.O Sistemas <caiosan321@icloud.com>
2026-02-02 12:14:44 -0300

    Update database.py

--- database.py
+++ database.py
@@ -2,7 +2,34 @@ import psycopg2
 import os
 
 def get_db_connection():
-    return psycopg2.connect(
+    conn = psycopg2.connect(
         os.environ["DATABASE_URL"],
         sslmode="require"
     )
+    criar_tabelas(conn)
+    return conn
+
+
+def criar_tabelas(conn):
+    cur = conn.cursor()
+
+    # CLIENTES
+    cur.execute("""
+        CREATE TABLE IF NOT EXISTS clientes (
+            id SERIAL PRIMARY KEY,
+            nome VARCHAR(100) NOT NULL,
+            telefone VARCHAR(30)
+        )
+    """)
+
+    # SERVIÇOS
+    cur.execute("""
+        CREATE TABLE IF NOT EXISTS servicos (
+            id SERIAL PRIMARY KEY,
+            nome VARCHAR(100) NOT NULL,
+            valor NUMERIC(10,2) NOT NULL
+        )
+    """)
+
+    conn.commit()
+    cur.close()
